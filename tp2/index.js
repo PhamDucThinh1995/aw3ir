0c2b2b7f33a32b9ba0d50572b9388c1d6826f548
PhamDucThinh1995 <73902346+PhamDucThinh1995@users.noreply.github.com>
2021-10-22 10:07:11 +0200

    Add files via upload
    Upload .js file and .css file

---
+++ tp2/index.js
@@ -0,0 +1,37 @@
+function validation() {
+    if (document.getElementById("nom").value.length < 5){
+        document.getElementById("error").innerHTML = "Le nom doit contenir au moins 5 caracteres";
+        document.getElementById("error").style.display = "initial";
+		document.getElementById("resultat").style.display = "none";
+    }
+    else if (document.getElementById("prénom").value.length < 5) {
+        cument.getElementById("error").innerHTML = "La prenom doit contenir au moins 5 caracteres";
+        document.getElementById("error").style.display = "initial";
+		document.getElementById("resultat").style.display = "none";
+    }
+    else if (document.getElementById("datedenaissance").value.length < 8) {
+        document.getElementById("error").innerHTML = "La date doit contenir au moins 8 caracteres";
+        document.getElementById("error").style.display = "initial";
+		document.getElementById("resultat").style.display = "none";
+    }
+    else if (document.getElementById("adresse").value.length < 5) {
+        document.getElementById("error").innerHTML = "L'adress doit contenir au moins 5 caracteres";
+        document.getElementById("error").style.display = "initial";
+		document.getElementById("resultat").style.display = "none";
+    }
+    else if (document.getElementById("email").value.length < 5) {
+        document.getElementById("error").innerHTML = "L'email doit contenir au moins 5 caracteres";
+        document.getElementById("error").style.display = "initial";
+		document.getElementById("resultat").style.display = "none";
+    }
+    else if(document.getElementById("email").value.includes("@")==0){
+        document.getElementById("error").innerHTML = "L'email ne valide pas";
+        document.getElementById("error").style.display = "initial";
+		document.getElementById("resultat").style.display = "none";
+    }
+    else    {
+        document.getElementById("resultat").innerHTML = "Bienvenue " + document.getElementById("prénom").value;
+        document.getElementById("error").style.display = "none";
+		document.getElementById("resultat").style.display = "initial";
+    }
+}
